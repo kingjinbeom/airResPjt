930ec529824330fe098689cd4dba7c8fb1817572
jinbeom <jinbeomgit@gmail.com>
2023-05-30 15:42:02 +0900

    HTML, CSS, db.js, change.js, session.js, util.js 까지 진행!

--- js/changeView.js
+++ js/changeView.js
@@ -14,51 +14,51 @@ let listWrap = "";
 
 /*회원가입, 로그인, 예약, 예약리스트 기능 실행시 위치 가져옴*/
 const initViews = () => {
-  console.log("initViews() CALLED!!");
+    console.log("initViews() CALLED!!");
 
-  signUpWrap = document.querySelector("div.sign_up_wrap");
-  signInWrap = document.querySelector("div.sign_in_wrap");
-  reservationWrap = document.querySelector("div.reservation_wrap");
-  listWrap = document.querySelector("div.list_wrap");
+    signUpWrap = document.querySelector("div.sign_up_wrap");
+    signInWrap = document.querySelector("div.sign_in_wrap");
+    reservationWrap = document.querySelector("div.reservation_wrap");
+    listWrap = document.querySelector("div.list_wrap");
 };
 
 /* 선택된 뷰만 보이게 하기 */
 const showSelectedView = (viewNO) => {
-  console.log("showSelectedView() CALLED!!");
-  switch (viewNO) {
-    case SIGN_UP_VIEW:
-      signUpWrap.style.display = "block";
-      signInWrap.style.display = "none";
-      reservationWrap.style.display = "none";
-      listWrap.style.display = "none";
-      break;
+    console.log("showSelectedView() CALLED!!");
+    switch (viewNO) {
+        case SIGN_UP_VIEW:
+            signUpWrap.style.display = "block";
+            signInWrap.style.display = "none";
+            reservationWrap.style.display = "none";
+            listWrap.style.display = "none";
+            break;
 
-    case SIGN_IN_VIEW:
-      signUpWrap.style.display = "none";
-      signInWrap.style.display = "block";
-      reservationWrap.style.display = "none";
-      listWrap.style.display = "none";
-      break;
+        case SIGN_IN_VIEW:
+            signUpWrap.style.display = "none";
+            signInWrap.style.display = "block";
+            reservationWrap.style.display = "none";
+            listWrap.style.display = "none";
+            break;
 
-    case SIGN_OUT_VIEW:
-      signUpWrap.style.display = "none";
-      signInWrap.style.display = "block";
-      reservationWrap.style.display = "none";
-      listWrap.style.display = "none";
-      break;
+        case SIGN_OUT_VIEW:
+            signUpWrap.style.display = "none";
+            signInWrap.style.display = "block";
+            reservationWrap.style.display = "none";
+            listWrap.style.display = "none";
+            break;
 
-    case RESERVATION_VIEW:
-      signUpWrap.style.display = "none";
-      signInWrap.style.display = "none";
-      reservationWrap.style.display = "block";
-      listWrap.style.display = "none";
-      break;
+        case RESERVATION_VIEW:
+            signUpWrap.style.display = "none";
+            signInWrap.style.display = "none";
+            reservationWrap.style.display = "block";
+            listWrap.style.display = "none";
+            break;
 
-    case RES_LIST_VIEW:
-      signUpWrap.style.display = "none";
-      signInWrap.style.display = "none";
-      reservationWrap.style.display = "none";
-      listWrap.style.display = "block";
-      break;
-  }
+        case RES_LIST_VIEW:
+            signUpWrap.style.display = "none";
+            signInWrap.style.display = "none";
+            reservationWrap.style.display = "none";
+            listWrap.style.display = "block";
+            break;
+    }
 };
--- js/util.js
+++ js/util.js
@@ -1,16 +1,16 @@
-let days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+// let days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 
-const getCurrentDateTime = () => {
-  console.log("getCurrentDateTime CALLED!!");
+// const getCurrentDateTime = () => {
+//   console.log("getCurrentDateTime CALLED!!");
 
-  let today = new Date();
-  let year = today.getFullYear();
-  let month = today.getMonth() + 1; //0: 1월, 1: 2월...
-  let date = today.getDate();
-  let day = today.getDay(); // 0: 일요일
+//   let today = new Date();
+//   let year = today.getFullYear();
+//   let month = today.getMonth() + 1; //0: 1월, 1: 2월...
+//   let date = today.getDate();
+//   let day = today.getDay(); // 0: 일요일
 
-  return `[${year}/${month}/${date}/ ${days[day]}]`;
-};
+//   return `[${year}/${month}/${date}/ ${days[day]}]`;
+// };
 
 // 개발단계(true), 배포단계(false)
 // const consoleFlag = true;
